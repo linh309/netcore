74e83f04c05d7a8ee22b18ea552c92af82eb7ad4
Linh Nguyen <linh309@gmail.com>
2019-04-26 09:18:16 +0700

    install more nuget packages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace dotnet_core_new
 {
@@ -7,6 +10,19 @@ namespace dotnet_core_new
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Startup 
+    {
+        public void Configure (IApplicationBuilder app) 
+        {
+            var now = DateTime.Now.ToString("t");
+            var message = "Now is: " + now;
+
+            app.Run(async (context) =>{
+                await context.Response.WriteAsync(message);
+            });
         }
     }
 }
